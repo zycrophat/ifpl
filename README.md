6096d369fda1c79cb93fd2521eeb7e2f240e5010
Andreas Steffan <andreas.steffan@mailbox.org>
2019-04-28 19:13:28 +0200

    added initial version of ifpl

---
+++ README.md
@@ -0,0 +1,24 @@
+ifpl -- if process lives
+========================
+
+Description
+-----------
+
+ifpl runs the given command and waits for the process with pid \<pid> to terminate.
+Upon termination of the process with pid \<pid> the spawned process will be killed.
+
+Usage
+-----
+
+`ifpl.exe [-help] [-pid <pid>] CMD [ARGS ...]`
+
+Where:
+
+  - `-help` displays a help message
+  - `-pid <pid>` specifies the pid of the process to wait for to terminate. Defaults to ppid of ifpl.
+  - `CMD [ARGS ...]` specifies the CMD and optionally arguments
+
+Compatibility
+-------------
+
+Currently, ifpl only runs on Microsoft Windows.
---
+++ cmd/ifpl/ifpl.go
@@ -0,0 +1,145 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Andreas Steffan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+)
+
+const (
+	sleepDuration  = time.Duration(1000) * time.Millisecond
+	exitCodeOffset = 40000
+)
+
+const (
+	pidFlagName  = "pid"
+	helpFlagName = "help"
+)
+
+func main() {
+	ifplArgs := parseArgs()
+	if ifplArgs.help {
+		printHelp()
+		os.Exit(0)
+	}
+
+	if ifplArgs.cmdName == "" {
+		printHelp()
+		os.Exit(exitCodeOffset + 1)
+	}
+
+	cmd := createCmd(ifplArgs.cmdName, ifplArgs.cmdArgs)
+
+	err := cmd.Start()
+	if err != nil {
+		os.Exit(exitCodeOffset + 2)
+	}
+
+	go waitForPidToTerminateAndKillProcess(ifplArgs.pid, cmd.Process)
+
+	_ = cmd.Wait()
+
+	os.Exit(cmd.ProcessState.ExitCode())
+}
+
+func printHelp() {
+	fmt.Print("ifpl -- if process lives\n")
+	fmt.Printf("(pid: %d, ppid %d)\n\n", os.Getpid(), os.Getppid())
+
+	fmt.Print("Usage:\n")
+	fmt.Printf("ifpl [-%s] [-%s <pid>] CMD [ARGS ...]\n", helpFlagName, pidFlagName)
+	flag.PrintDefaults()
+
+	fmt.Print("\nifpl runs the given CMD and waits for the process with pid <pid> to terminate. " +
+		"Upon termination CMD will be killed.\n\n")
+
+}
+
+type ifplArgs struct {
+	pid     int      // the pid to wait for to terminate
+	cmdName string   // the cmd to execute
+	cmdArgs []string // args for the cmd to execute
+
+	help bool // flag to print help
+}
+
+func parseArgs() ifplArgs {
+	pid := flag.Int(pidFlagName, os.Getppid(), "the pid to wait for to terminate. "+
+		"Defaults to ppid of ifpl")
+	helpIsRequested := flag.Bool(helpFlagName, false, "displays this help message")
+	flag.Parse()
+	args := flag.Args()
+
+	ifplArgs := ifplArgs{
+		pid: *pid,
+		cmdName: func() string {
+			if len(args) > 0 {
+				return args[0]
+			} else {
+				return ""
+			}
+		}(),
+		cmdArgs: func() []string {
+			if len(args) > 1 {
+				return args[1:]
+			} else {
+				return []string{}
+			}
+		}(),
+		help: *helpIsRequested,
+	}
+	return ifplArgs
+}
+
+func createCmd(cmdName string, cmdArgs []string) *exec.Cmd {
+	cmd := exec.Command(cmdName, cmdArgs...)
+	configureCmd(cmd)
+	return cmd
+}
+
+func configureCmd(cmd *exec.Cmd) {
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+}
+
+func waitForPidToTerminateAndKillProcess(pid int, process *os.Process) {
+	for {
+		processToWaitFor, err := os.FindProcess(pid)
+
+		if err == nil {
+			// on Windows we can wait for arbitrary process to terminate
+			_, _ = processToWaitFor.Wait()
+			_ = process.Kill()
+			return
+		}
+
+		time.Sleep(sleepDuration)
+	}
+}
